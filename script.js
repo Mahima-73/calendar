894ec75276818fa42b58662b064451eec6a34f21
Mahima-73 <sharma.mahima514@gmail.com>
2024-12-22 13:46:27 +0530

    calendar is done

--- script.js
+++ script.js
@@ -31,10 +31,53 @@ function initCalendar() {
     const prevLastDay = new Date(year, month, 0);
     const prevDays = prevLastDay.getDate();
     const lastDate = lastDay.getDate(); 
-    const day = firstDay.getDate();
+    const day = firstDay.getDay();
     const nextDays = 7 - lastDay.getDay() - 1;
 
     //update date top of calendar
     date.innerHTML = months[month] + " " + year;
-
+    //adding days on dom
+    let days = "";
+    //prev month days
+    for(let x = day; x > 0; x--){
+        days += `<div class ="day prev-date" > ${prevDays -x + 1}</div>`;
+    }
+    //current month days
+    for (let i=1; i<=lastDate; i++){
+        //if day is today add class today
+        if(
+            i === new Date().getDate()&&
+            year === new Date().getFullYear()&&
+            month === new Date().getMonth()
+        ) {        
+            days += `<div class ="day today" > ${i}</div>`;
+    }  //add remaing as it is
+    else{
+        days += `<div class="day"> ${i}</div>`;
+    }}
+    //next month days
+    for (let j=1; j <= nextDays; j++){
+        days += `<div class="day next-date">${j}</div>`
+    }
+    daysContainer.innerHTML = days;
 }initCalendar();
+
+//prev month
+function prevMonth(){
+    month--;
+    if (month<0){
+        month =11;
+        year--;
+    }initCalendar();
+}
+//next month
+function nextMonth(){
+    month++;
+    if (month > 11) {
+        month = 0;
+        year++;
+    }initCalendar();
+} //eventListerner to go to prev month and next month
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth); //calendar done here
+//now goto and today button
